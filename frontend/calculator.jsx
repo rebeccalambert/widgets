92ded2146288b54223482e3db610c1449c7decbc
Rebecca Lambert <rebeccalambert@berkeley.edu>
2020-02-05 17:37:17 -0800

    Add basic functions - addition isn't working right

--- frontend/calculator.jsx
+++ frontend/calculator.jsx
@@ -9,14 +9,64 @@ class Calculator extends React.Component {
         num1 : "",
         num2 : "",
     }
+
+    this.setNum1 = this.setNum1.bind(this);
+    this.setNum2 = this.setNum2.bind(this);
+
+    this.add = this.add.bind(this);
+    this.subtract = this.subtract.bind(this);
+    this.multiply = this.multiply.bind(this);
+    this.divide = this.divide.bind(this);
+
   }
 
-  // your code here
+  setNum1(e){
+    const num1 = e.target.value;
+    this.setState({ num1 });
+  }
+
+  setNum2(e){
+    const num2 = e.target.value;
+    this.setState({ num2 });
+  }
+
+
+  add(e){
+    e.preventDefault();
+    const result = this.state.num1 + this.state.num2;
+    this.setState({ result });
+  }
+
+  subtract(e){
+    e.preventDefault();
+    const result = this.state.num1 - this.state.num2;
+    this.setState({ result });
+  }
+
+  multiply(e){
+    e.preventDefault();
+    const result = this.state.num1 * this.state.num2;
+    this.setState({ result });
+  }
+
+  divide(e){
+    e.preventDefault();
+    const result = this.state.num1 / this.state.num2;
+    this.setState({ result });
+  }
 
   render() {
     return (
       <div>
-        <h1>Hello World</h1> // replace this with your code
+        <h1>{this.state.result}</h1>
+
+        <input onChange={this.setNum1} value={this.state.num1}/>
+        <input onChange={this.setNum2} value={this.state.num2}/>
+        <br />
+        <button onClick={this.add}>+</button>
+        <button onClick={this.subtract}>-</button>
+        <button onClick={this.multiply}>*</button>
+        <button onClick={this.divide}>/</button>
       </div>
     );
   }
