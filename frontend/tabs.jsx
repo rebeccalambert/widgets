599ef8cc4a365cf15c409dfd15a2ff19483891ce
Rebecca Lambert <rebeccalambert@berkeley.edu>
2020-02-05 17:27:46 -0800

    Create tabs component skeleton

---
+++ frontend/tabs.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default class Tabs extends React.Component {
+
+  constructor(tabs) {
+    super();
+
+    this.state = {
+        index : 0,
+    }
+    
+  }
+
+  render() {
+  }
+}
